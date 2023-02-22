779d70e9696948ad982b273790d781dd7e5b8a12
= <=>
2023-02-22 15:18:55 -0700

    added migrations

--- learnit_app/__init__.py
+++ learnit_app/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from learnit_app.config import Config
+from flask_migrate import Migrate
 
 # server setup
 app = Flask(__name__)
@@ -11,3 +12,4 @@ app.secret_key = os.urandom(24)
 
 # database setup
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
